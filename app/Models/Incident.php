9ac67cc3c084d23c02d1c99b04e9694fb779fb88
dan <dan.nagaoka.0@gmail.com>
2020-11-09 07:04:43 -0300

    Add relationships

--- app/Models/Incident.php
+++ app/Models/Incident.php
@@ -4,10 +4,39 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Validation\Rule;
 
 class Incident extends Model
 {
     use HasFactory;
+
+    public $fillable = [
+        'location_id',
+        'incident_type_id',
+        'occurrence_time',
+        'severity',
+        'description',
+    ];
+
+    /**
+     * Gets the type to which the incident belongs.
+     * @return BelongsTo 
+     */
+    public function incidentType()
+    {
+        return $this->belongsTo(IncidentType::class);
+    }
+
+    /**
+     * Gets the location where the incident occurred.
+     * @return BelongsTo 
+     */
+    public function location()
+    {
+        return $this->belongsTo(Location::class);
+    }
 
     public const SEVERITY_LEVELS = [
         'least_concern',
@@ -15,5 +44,24 @@ class Incident extends Model
         'acute',
         'critical',
         'catastrophe',
+    ];
+
+    public const RULES = [
+        'location_id' => [
+            'required',
+            'integer',
+            'exists:' . Location::class . ',id',
+        ],
+        'incident_type_id' => [
+            'required',
+            'integer',
+            'exists:' . IncidentType::class . ',id',
+        ],
+        'occurrence_time' => 'required|date',
+        'severity' => [
+            'required',
+            'in_array' => self::SEVERITY_LEVELS,
+        ],
+        'description' => 'string',
     ];
 }
